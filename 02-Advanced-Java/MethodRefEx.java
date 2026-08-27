508242eb57b740c1fe935de231f4f4efc8d3ffe5
SIVAPRASAD <avisdasarp03@gmail.com>
2026-08-27 16:05:12 +0530

    Add MethodRefEx example with Student constructor reference

---
+++ 02-Advanced-Java/MethodRefEx.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class MethodRefEx {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Siva", "Avis", "Prasad", "Darsap");
+
+        List<Student> students = names.stream()
+                .map(Student::new)
+                .toList();
+
+        System.out.println(students);
+    }
+}
+
+class Student {
+    private String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{name='" + name + "'}";
+    }
+}
